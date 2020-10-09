4e0d22eb3d21c34616e4836897d27b0aecd3f425
thomas ledesert <thomasledesert@MacBook-Air-de-thomas.home>
2020-10-09 11:31:41 +0200

    fix redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        
         if (! $request->expectsJson()) {
-            return route('/');
+            return response()->json(['error' => 'Unauthenticated.'], 401);
         }
     }
 }
